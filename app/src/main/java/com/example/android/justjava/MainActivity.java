af5873f6ccfb04a9fe366b31d378c878ce7cf299
Tomasz Kwiatkowski <tomus19@gmail.com>
2018-02-03 11:58:21 +0100

    update

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -10,10 +10,16 @@ package com.example.android.justjava;
 
 
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import java.text.NumberFormat;
 
 /**
@@ -23,6 +29,8 @@ public class MainActivity extends AppCompatActivity
 {
     int quantity = 0;
     int price = 5;
+    int whippedCreamPrice = 1;
+    int chocolatePrice = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,15 +42,45 @@ public class MainActivity extends AppCompatActivity
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        String priceMessage = "Total: $" + price*quantity + "\nThank you!";
-        displayMessage(priceMessage);
+        CheckBox a = findViewById(R.id.whipped_cream_checkbox);
+        CheckBox b = findViewById(R.id.chocolate_checkbox);
+        EditText nameBox = findViewById(R.id.nameBox);
+        String name = nameBox.getText().toString();
+        boolean hasCream = a.isChecked();
+        boolean hasChocolate = b.isChecked();
+        int total = countPrice(hasCream, hasChocolate);
+
+        String priceMessage = "Welcome " + name + ", your order is:";
+        priceMessage += "\nTotal: $" + total + "\nThank you!";
+        priceMessage += "\nWith Whipped Cream : " + hasCream;
+        priceMessage += "\nWith Chocolate : " + hasChocolate;
+//        displayMessage(priceMessage);
+
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:"));
+        intent.putExtra(intent.EXTRA_TEXT, priceMessage);
+        if(intent.resolveActivity(getPackageManager()) != null)
+            startActivity(intent);
+    }
+
+    int countPrice(boolean a, boolean b)
+    {
+        int total = price;
+        if(a == true)
+            total += whippedCreamPrice;
+        if(b == true)
+            total += chocolatePrice;
+        return total*quantity;
     }
 
     /**
      * This method is called when the + button is clicked.
      */
     public void increment(View view) {
-        quantity++;
+        if(quantity<100)
+            quantity++;
+        else
+            Toast.makeText(getApplicationContext(), "Too much coffee!", Toast.LENGTH_SHORT).show();
         display(quantity);
     }
 
@@ -50,7 +88,10 @@ public class MainActivity extends AppCompatActivity
      * This method is called when the - button is clicked.
      */
     public void decrement(View view) {
-        quantity--;
+        if(quantity>1)
+            quantity--;
+        else
+            Toast.makeText(getApplicationContext(), "At least 1 coffee required!", Toast.LENGTH_SHORT).show();
         display(quantity);
     }
 
@@ -60,14 +101,6 @@ public class MainActivity extends AppCompatActivity
     private void display(int number) {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
         quantityTextView.setText("" + number);
-    }
-
-    /**
-     * This method displays the given price on the screen.
-     */
-    private void displayPrice(int number) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
     }
 
     /**
